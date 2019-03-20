10043165a9f3f5c7ff4f93f88e4bd58070828765
Jessica Granziera <Jessica_gr@msn.com>
2019-03-20 13:03:19 +0100

    simple logger

---
+++ SimpleLogger/LoggerTester/Program.cs
@@ -0,0 +1,20 @@
+﻿using SimpleLogger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = null;
+
+            logger.LogInfo("Messaggio informativo");
+            logger.LogError("Errore", new Exception("Error"));
+        }
+    }
+}
